ef34038910fa5a42abc5448c71ff69fe7eb1c3c8
NipunaNiranjan <62852850+NipunaNiranjan@users.noreply.github.com>
2022-03-18 13:48:27 +0530

    Spring Initializer Project Added with some dependancies

---
+++ QuestionnaireApp/QuestionnaireApp/src/test/java/com/example/QuestionnaireApp/QuestionnaireAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuestionnaireApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
