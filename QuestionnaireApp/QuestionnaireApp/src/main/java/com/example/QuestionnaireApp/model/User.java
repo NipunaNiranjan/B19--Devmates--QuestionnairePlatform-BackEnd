d1cb0e207f50f0278018e6ada383a6455480465f
achintha <achinthathennakoon97@gmail.com>
2022-06-11 15:36:00 +0530

    added models related to user

---
+++ QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/model/User.java
@@ -0,0 +1,34 @@
+package com.example.QuestionnaireApp.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @ManyToOne(
+            targetEntity = Role.class,
+            optional = false
+    )
+    private Role role;
+}
