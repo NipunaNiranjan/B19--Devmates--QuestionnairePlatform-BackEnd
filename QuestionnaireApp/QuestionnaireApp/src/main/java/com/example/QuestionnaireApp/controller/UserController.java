778e6c21d28346f3ba161b6d6f676edbfaa26435
achintha <achinthathennakoon97@gmail.com>
2022-06-12 09:01:39 +0530

    add get all user request

--- QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/controller/UserController.java
+++ QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/controller/UserController.java
@@ -1,4 +1,23 @@
 package com.example.QuestionnaireApp.controller;
 
+import com.example.QuestionnaireApp.model.User;
+import com.example.QuestionnaireApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/getUsers")
+    public List<User> getUser () {
+        return userRepository.findAll();
+    }
 }
