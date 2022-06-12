07bca0fc5901dd4e098f74f62bfc211d93a9fc2d
achintha <achinthathennakoon97@gmail.com>
2022-06-12 15:23:41 +0530

    added get request to view users and role

--- QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/controller/UserController.java
+++ QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.QuestionnaireApp.controller;
 
 import com.example.QuestionnaireApp.model.User;
+import com.example.QuestionnaireApp.model.views.UserViews;
 import com.example.QuestionnaireApp.repository.UserRepository;
+import com.example.QuestionnaireApp.repository.UserViewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,8 +18,16 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private UserViewRepository userViewRepository;
+
     @GetMapping("/getUsers")
     public List<User> getUser () {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/admin/view_users")
+    public List<UserViews> getUsers () {
+        return userViewRepository.findAll();
     }
 }
---
+++ QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/model/views/UserViews.java
@@ -0,0 +1,37 @@
+package com.example.QuestionnaireApp.model.views;
+
+
+import com.example.QuestionnaireApp.model.ERole;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Immutable;
+
+import javax.persistence.*;
+
+@Entity
+@Immutable
+@Table(name = "view_users")
+@Setter
+@Getter
+@NoArgsConstructor
+public class UserViews {
+
+    @Id
+    private Long id;
+
+    @Column
+    private String username;
+
+    @Column
+    private boolean status;
+
+    @Column
+    @Enumerated(value = EnumType.STRING)
+    private ERole role;
+
+    public UserViews(ERole role) {
+        this.role = role;
+    }
+
+}
