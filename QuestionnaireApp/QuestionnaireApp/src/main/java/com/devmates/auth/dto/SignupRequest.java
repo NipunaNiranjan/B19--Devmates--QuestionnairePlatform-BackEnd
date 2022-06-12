ed15c38e691517f6b86bb58919ab2c74575f4acc
lakshan-maduwantha <weerasinghahplm.19@uom.lk>
2022-06-12 22:12:15 +0530

    import files

--- QuestionnaireApp/QuestionnaireApp/src/main/java/com/devmates/auth/dto/SignupRequest.java
+++ QuestionnaireApp/QuestionnaireApp/src/main/java/com/devmates/auth/dto/SignupRequest.java
@@ -25,6 +25,13 @@ public class SignupRequest {
     @StrongPassword(message = "{Strong.password}")
     private String password;
 
+    @Email(message = "Email is not valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+    @NotBlank(message = "{NotEmpty.Email}")
+    private String email;
+
+    @NotBlank(message = "{NotEmpty.role}")
+    private String role;
+
     @NotBlank(message = "{NotEmpty.role}")
     private String role;
 
