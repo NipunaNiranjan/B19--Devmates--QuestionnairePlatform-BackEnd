fe688d7168255c267e96e6fc6d345ebe97737ff8
lakshan-maduwantha <weerasinghahplm.19@uom.lk>
2022-06-13 07:07:27 +0530

    add dto class properties

---
+++ QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/dto/ClassDTO.java
@@ -0,0 +1,16 @@
+package com.example.QuestionnaireApp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ClassDTO {
+    private long Class_id;
+    private String name;
+    private long TeacherID;
+
+
+}
--- QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/model/Class.java
+++ QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/model/Class.java
@@ -1,7 +1,22 @@
 package com.example.QuestionnaireApp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+
 public class Class {
+    @Id
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
     private long Class_id;
     private String name;
     private long TeacherID;
+
+
 }
