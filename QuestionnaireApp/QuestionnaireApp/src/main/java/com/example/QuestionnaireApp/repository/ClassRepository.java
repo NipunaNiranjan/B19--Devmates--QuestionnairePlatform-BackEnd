d36fdd28f75ae50e72027ac653185fdcd123bc94
lakshan-maduwantha <weerasinghahplm.19@uom.lk>
2022-06-13 11:45:07 +0530

    add class repo

--- QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/repository/ClassRepository.java
+++ QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/repository/ClassRepository.java
@@ -2,5 +2,5 @@ package com.example.QuestionnaireApp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ClassRepository extends JpaRepository {
+public interface ClassRepository extends JpaRepository<Class, Long> {
 }
