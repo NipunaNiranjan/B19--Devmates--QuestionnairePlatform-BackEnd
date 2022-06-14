8c0656995189090ef8aa1c1cd3c3df031aec1b18
lakshan-maduwantha <weerasinghahplm.19@uom.lk>
2022-06-14 13:13:11 +0530

    chanage files

--- QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/repository/ClassRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.QuestionnaireApp.repository;
-
-import com.example.QuestionnaireApp.model.Class;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClassRepository extends JpaRepository<Class,Integer> {
-}
