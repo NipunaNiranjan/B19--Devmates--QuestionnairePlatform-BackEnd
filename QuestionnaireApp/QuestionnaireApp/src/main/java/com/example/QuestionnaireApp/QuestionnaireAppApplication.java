3e1d7baee50253484fc9844eabe2b1d0537d2ff2
rashmika <thilinirashmika098@gmail.com>
2022-06-19 23:00:16 +0530

    Changed conflicted files

--- QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/QuestionnaireAppApplication.java
+++ QuestionnaireApp/QuestionnaireApp/src/main/java/com/example/QuestionnaireApp/QuestionnaireAppApplication.java
@@ -1,9 +1,9 @@
 package com.example.QuestionnaireApp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 public class QuestionnaireAppApplication {
